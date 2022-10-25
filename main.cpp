72d32fe8795fb2beec540fa3f5706da8e0019a3a
Mohamad Yazan Musahher <mohamed.musahher@gmail.com>
2022-10-25 23:25:47 +0200

    Creat a new structure

--- main.cpp
+++ main.cpp
@@ -2,109 +2,109 @@
 
 using namespace std;
 
-class BST {
-private:
-    struct Node{
-        int data;
-        Node* left;
-        Node* right;
-    };
-    Node* root = nullptr;
-
-    // addHelper is same function as "add" but it is on Private section to hide hard code, and it should know where to put every node even to right or left form the value of Node
-
-    void addHelper(Node* temp, int value){
-        if (value <= temp->data) {
-            if(temp->left == nullptr){
-                Node* newNode = new Node;
-                newNode->data = value;
-                newNode->left = nullptr;
-                newNode->right = nullptr;
-
-                temp->left = newNode;
-
-            }
-            else {
-                addHelper(temp->left, value);
-            }
-
-        }
-        else {
-            if(temp->right == nullptr){
-                Node* newNode = new Node;
-                newNode->data = value;
-                newNode->left = nullptr;
-                newNode->right = nullptr;
-
-                temp->right = newNode;
-
-            }
-            else {
-                addHelper(temp->right, value);
-            }
-
-        }
-
-    }
-
-
-public:
-
-    //creat a new Node
-    void add(int value){
-        if (root == nullptr){
-            Node* newNode = new Node;
-            newNode->data = value;
-            newNode->right = nullptr;
-            newNode->left = nullptr;
-
-            root = newNode;
-        } else {
-            addHelper(root, value);
-        }
-    }
-
-    //finde Max verdi i tree
-
-    int getMax(){
-        Node* temp = root;
-        while(temp->right != nullptr){
-            temp = temp->right;
-        }
-        return temp->data;
-    }
-
-    //finde Min verdi i tree
-    int getMin(){
-        Node* temp = root;
-        while(temp->left != nullptr){
-            temp = temp->left;
-        }
-        return temp->data;
-    }
-
-// Display whole Tree
-    void PrinTree (Node* temp) {
-        if (root == nullptr) return;
-        PrinTree(root->left);
-        PrinTree(root->right);
-        cout<<root->data <<endl;
-    }
-
+enum Type {
+    Horizontal,
+    Vertical,
+    Boundary,
 };
 
+class Rectangle {
+public:
+    int minimum_width;
+    int minimum_height;
 
-int main () {
+    Rectangle() {
+        this->minimum_width = 0;
+        this->minimum_height = 0;
+    }
 
-    BST object;
-    object.add(13);
-    object.add(1);
-    object.add(3);
-    object.add(5);
-    object.add(7);
+    Rectangle(int minimum_width, int minimum_height) {
+        this->minimum_width = minimum_width;
+        this->minimum_height = minimum_height;
+    }
+};
 
-    cout<<object.getMax()<<endl;
-    cout<<object.getMin()<<endl;
+class Node {
+public:
+    Type type;
+    Rectangle boundary;
 
+    int key;
+    Node *left;
+    Node *right;
+
+    Node(Type type, Rectangle boundary, int key) {
+        this->type = type;
+        this->boundary = boundary;
+        this->key = key;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+};
+
+class BinaryTree {
+public:
+    Node *root;
+
+    void add(Node *base, Type type, Rectangle boundary, int key) {
+        if (root) {
+            if (key <= base->key) {
+                if (base->left) {
+                    add(base->left, type, boundary, key);
+                } else {
+                    base->left = new Node(type, boundary, key);
+                }
+            } else {
+                if (base->right) {
+                    add(base->right, type, boundary, key);
+                } else {
+                    base->right = new Node(type, boundary, key);
+                }
+            }
+        } else {
+            root = new Node(type, boundary, key);
+        }
+    }
+
+    void addSlice(Type type, int key) {
+        add(this->root, type, Rectangle(0, 0), key);
+    }
+
+    void addRectangle(Rectangle rectangle, int key) {
+        add(this->root, Boundary, rectangle, key);
+    }
+
+    void printNode(Node *base, int indent) {
+        if (base) {
+            // Print indent
+            cout << string(indent, ' ') << "|-> ";
+
+            if (base->type == Boundary)
+                cout << base->boundary.minimum_width << "x" << base->boundary.minimum_height << endl;
+
+            if (base->type == Horizontal)
+                cout << "H" << endl;
+
+            if (base->type == Vertical)
+                cout << "V" << endl;
+
+            printNode(base->left, indent + 4);
+            printNode(base->right, indent + 4);
+        }
+    }
+
+    void print() {
+        printNode(this->root, 0);
+    }
+};
+
+int main() {
+    BinaryTree binary_tree = BinaryTree();
+    binary_tree.addSlice(Vertical, 0);
+    binary_tree.addRectangle(Rectangle(1,1), -1);
+    binary_tree.addSlice(Horizontal, 2);
+    binary_tree.addRectangle(Rectangle(2,2), 1);
+    binary_tree.addRectangle(Rectangle(3,3), 3);
+    binary_tree.print();
     return 0;
 }
